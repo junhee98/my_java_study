caba34f08c35762cca65f6bb74d008d83dc73be7
junhee98 <62004821+junhee98@users.noreply.github.com>
2020-05-26 17:27:40 +0900

    Update BinaryTest2.java

--- Chapter2/src/binary/BinaryTest2.java
+++ Chapter2/src/binary/BinaryTest2.java
@@ -3,10 +3,10 @@ package binary;
 public class BinaryTest2 {
 
 	public static void main(String[] args) {
-		int num1 = 0B00000000000000000000000000000101; //2Áø¼ö 5
-		int num2 = 0B11111111111111111111111111111011; //2Áø¼ö -5
+		int num1 = 0B00000000000000000000000000000101; //2ì§„ìˆ˜ 5
+		int num2 = 0B11111111111111111111111111111011; //2ì§„ìˆ˜ -5
 		
-		int sum = num1 + num2; //num1 °ú num2¸¦ ´õÇÑ´Ù.
+		int sum = num1 + num2; //num1 ê³¼ num2 ì„ .
 		System.out.println(num1);
 		System.out.println(num2);
 		System.out.println(sum);
